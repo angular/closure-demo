7a8a1248d1d832d341787d6548f06e4251483a21
Alex Eagle <alexeagle@google.com>
2016-05-13 08:37:51 -0700

    get the SIMPLE_OPTIMIZATIONS working
    Had to move the generated sources to the src directory since the module names need to match
    Now start trying with advanced optimizations... broken :(

--- build_closure.sh
+++ build_closure.sh
@@ -1,10 +1,16 @@
 OPTS=(
   "--language_in=ES6_STRICT"
   "--language_out=ES5"
-  "--compilation_level=SIMPLE_OPTIMIZATIONS"
-  "built/{src,gen}/*.js"
-  $(find node_modules/@angular/{core,common,platform-browser}/esm -name *.js)
+  "--compilation_level=ADVANCED_OPTIMIZATIONS"
+  "--js_output_file=dist/bundle.js"
+  "--entry_point=built\$bootstrap"
+  "--variable_renaming_report=dist/variable_renaming_report"
+  "--property_renaming_report=dist/property_renaming_report"
+  "built/*.js"
+  $(find node_modules/@angular/{core,common,compiler,platform-browser}/esm -name *.js)
   $(find node_modules/rxjs-es -name *.js)
 )
 set -ex
 java -jar node_modules/google-closure-compiler/compiler.jar $(echo ${OPTS[*]})
+gzip --keep -f dist/bundle.js
+ls -alH dist
