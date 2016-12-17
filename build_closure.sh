63241512a2543f2943674fc58f27dfeb36ceedff
Alex Eagle <alexeagle@google.com>
2016-12-16 16:23:36 -0800

    turn off closure polyfill rewrites

--- build_closure.sh
+++ build_closure.sh
@@ -3,19 +3,28 @@ OPTS=(
   "--language_out=ES5"
   "--compilation_level=ADVANCED_OPTIMIZATIONS"
   "--js_output_file=dist/bundle.js"
-  "--entry_point=./built/bootstrap"
   "--create_source_map=%outname%.map"
   "--variable_renaming_report=dist/variable_renaming_report"
   "--property_renaming_report=dist/property_renaming_report"
+
+  # Don't include ES6 polyfills
+  "--rewrite_polyfills=false"
+
   # List of path prefixes to be removed from ES6 & CommonJS modules.
   "--js_module_root=node_modules"
   "--js_module_root=vendor"
+
+  # Uncomment for easier debugging
+  # "--formatting=PRETTY_PRINT"
+
   node_modules/zone.js/dist/zone.js
   $(find vendor/rxjs -name *.js)
   node_modules/@angular/{core,common,compiler,platform-browser}/index.js
   $(find node_modules/@angular/{core,common,compiler,platform-browser}/src -name *.js)
   "built/*.js"
+  "--entry_point=./built/bootstrap"
 )
+
 set -ex
 java -jar node_modules/google-closure-compiler/compiler.jar $(echo ${OPTS[*]})
 gzip --keep -f dist/bundle.js
