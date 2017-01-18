6283a9d447ad7b7a3e97e3d2fc23f59b68e6499e
Alex Eagle <alexeagle@google.com>
2017-01-17 21:49:50 -0800

    make the app work again: add missing Zone.js externs

--- build_closure.sh
+++ build_closure.sh
@@ -35,8 +35,12 @@ OPTS=(
   "--output_manifest=dist/manifest.MF"
 )
 
+JVM_ARGS=""
+# To attach a remote debugger:
+#JVM_ARGS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+
 set -ex
-java -jar node_modules/google-closure-compiler/compiler.jar $(echo ${OPTS[*]})
+java $JVM_ARGS -jar node_modules/google-closure-compiler/compiler.jar $(echo ${OPTS[*]})
 
 # measure the sizes of scripts the user will need to load
 for script in dist/bundle.js node_modules/zone.js/dist/zone.min.js; do
