9584a2317fb47d9d8f7ffcd67b07c701f2b6efc3
Alex Eagle <alexeagle@google.com>
2017-04-17 20:55:24 -0700

    install brotli in the docker image, report its size again

--- size_report.sh
+++ size_report.sh
@@ -1,3 +1,12 @@
+# Distributions of brotli disagree on the name of the command
+run_brotli() {
+    if hash brotli 2>/dev/null; then
+        brotli "$@"
+    else
+        bro "$@"
+    fi
+}
+
 # measure the sizes of scripts the user will need to load
 for script in dist/bundle.js node_modules/zone.js/dist/zone.min.js; do
   # gzip on linux doesn't have --keep
@@ -6,6 +15,6 @@ for script in dist/bundle.js node_modules/zone.js/dist/zone.min.js; do
   mv $script.bak $script
   # requires brotli
   # on Mac: brew install brotli
-  #bro --force --quality 10 --input $script --output $script.brotli
-  ls -alH ${script}.gz # ${script}.brotli
+  run_brotli --force --quality 10 --input $script --output $script.brotli
+  ls -alH ${script}.gz ${script}.brotli
 done
